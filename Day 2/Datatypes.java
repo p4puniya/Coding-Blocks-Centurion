20d497baebe7ba6ce1843e6c04e0f015dc88a528
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-05-15 13:59:16 +0530

    datatypes

---
+++ Day 2/Datatypes.java
@@ -0,0 +1,16 @@
+class Datatypes{
+    public static void main(String []args){
+        int a = 10;
+        float b = 10.5f;
+        double c = 10.5;
+        char d = 'A';
+        String e = "Hello";
+        boolean f = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
