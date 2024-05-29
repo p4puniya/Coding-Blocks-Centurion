e2678d0c42539c489abffa15cb2a12ef7ce28493
G_puniya <60355338+p4puniya@users.noreply.github.com>
2024-05-29 09:43:48 +0530

    Coin Flip

--- Week 3/DAy2/Recursion.java
+++ Week 3/DAy2/Recursion.java
@@ -33,9 +33,18 @@ class Recursion{
         subSeq(s.substring(1),ans+ch);
         subSeq(s.substring(1),ans);
     }
+    //Print all outcomes of a coin flip
+    static void coinFlip(int n, String ans){
+        if(n==0){
+            System.out.println(ans);
+            return;
+        }
+        coinFlip(n-1, ans+"H");
+        coinFlip(n-1, ans+"T");
+    }
     public static void main(String args[]){
         int n=5;
-        subSeq("CAR","");
+        coinFlip(3,"");
         // System.out.print();
     }
 }
